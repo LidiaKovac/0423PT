94ecb2b2af9a171f1d833b97f56f21c2ceba9082
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-10-09 20:04:30 +0200

    add parte 1

---
+++ M2/D1/script.js
@@ -0,0 +1,19 @@
+let ilMioNome  //vuota, undefined
+console.log(ilMioNome)
+ilMioNome = "Lidia"
+console.log(ilMioNome)
+//let => indica la CREAZIONE della variabile, dichiarazione
+let ilMioCognome = "Marco Rossi"
+//altro codice che prende l'input utente
+ilMioCognome = "input utente"
+
+let titoloApp = "Faceapp"
+
+let giornoDelMese = 9
+console.log(giornoDelMese)
+
+let base = 2
+let altezza = 2
+
+let areaDelRettangolo = base * altezza
+console.log(areaDelRettangolo)
