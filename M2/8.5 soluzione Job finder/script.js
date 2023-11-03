3202f82d1606dc7eafbcdc211ffbbf4b36fe82e5
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-11-03 21:10:13 +0100

    adfd alternative solution

---
+++ M2/8.5 soluzione Job finder/script.js
@@ -0,0 +1,117 @@
+const jobs = [
+    { title: "Marketing Intern", location: "US, NY, New York" },
+    {
+        title: "Customer Service - Cloud Video Production",
+        location: "NZ, Auckland",
+    },
+    {
+        title: "Commissioning Machinery Assistant (CMA)",
+        location: "US, IA, Wever",
+    },
+    {
+        title: "Account Executive - Washington DC",
+        location: "US, DC, Washington",
+    },
+    { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
+    { title: "Accounting Clerk", location: "US, MD," },
+    { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
+    {
+        title: "Lead Guest Service Specialist",
+        location: "US, CA, San Francisco",
+    },
+    { title: "HP BSM SME", location: "US, FL, Pensacola" },
+    {
+        title: "Customer Service Associate - Part Time",
+        location: "US, AZ, Phoenix",
+    },
+    {
+        title: "ASP.net Developer Job opportunity at United States,New Jersey",
+        location: "US, NJ, Jersey City",
+    },
+    {
+        title: "Talent Sourcer (6 months fixed-term contract)",
+        location: "GB, LND, London",
+    },
+    {
+        title: "Applications Developer, Digital",
+        location: "US, CT, Stamford",
+    },
+    { title: "Installers", location: "US, FL, Orlando" },
+    { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
+    {
+        title: "VP of Sales - Vault Dragon",
+        location: "SG, 01, Singapore",
+    },
+    { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
+    {
+        title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
+        location: "GB, SOS, Southend-on-Sea",
+    },
+    { title: "Visual Designer", location: "US, NY, New York" },
+    {
+        title: "Process Controls Engineer - DCS PLC MS Office - PA",
+        location: "US, PA, USA Northeast",
+    },
+    { title: "Marketing Assistant", location: "US, TX, Austin" },
+    { title: "Front End Developer", location: "NZ, N, Auckland" },
+    { title: "Engagement Manager", location: "AE," },
+    {
+        title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
+        location: "US, CA, Carlsbad",
+    },
+    { title: "Customer Service", location: "GB, LND, London" },
+    { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
+    { title: "Marketing Exec", location: "SG," },
+    {
+        title: "HAAD/DHA Licensed Doctors Opening in UAE",
+        location: "AE, AZ, Abudhabi",
+    },
+    {
+        title: "Talent Management Process Manager",
+        location: "US, MO, St. Louis",
+    },
+    { title: "Customer Service Associate", location: "CA, ON, Toronto" },
+    {
+        title: "Customer Service Technical Specialist",
+        location: "US, MA, Waltham",
+    },
+    { title: "Software Applications Specialist", location: "US, KS," },
+    { title: "Craftsman Associate", location: "US, WA, Everett" },
+    { title: "Completion Engineer", location: "US, CA, San Ramon" },
+    { title: "I Want To Work At Karmarama", location: "GB, LND," },
+    {
+        title: "English Teacher Abroad",
+        location: "US, NY, Saint Bonaventure",
+    },
+]
+
+
+
+const query = {
+    position: "",
+    location: ""
+}
+const ul = document.querySelector("ul")
+
+const getjobs = function (changeEvent) {
+    ul.innerHTML = ""
+    const inputValue = changeEvent.target.value.toLowerCase()
+    if (changeEvent.target.id === "position") {
+        query.position = inputValue
+    } else if (changeEvent.target.id === "location") {
+        query.location = inputValue
+    }
+    for (let i = 0; i < jobs.length; i++) {
+        const job = jobs[i]
+        if (job.title.toLowerCase().includes(query.position) 
+        && job.location.toLowerCase().includes(query.location)) {
+            ul.innerHTML += `<li> ${job.title} @${job.location}`
+        }
+    }
+}
+
+const position = document.querySelector("#position")
+const locationV = document.querySelector("#location")
+
+position.addEventListener("keyup", getjobs)
+locationV.addEventListener("keyup", getjobs)
